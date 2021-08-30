97f81d44f715f4bf9d9b84a0b9be7c7a42a8042d
Minsung <kms3700@gmail.com>
2021-08-30 13:22:59 +0900

    google placeautocomplete

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -9,54 +9,54 @@ import searchResults from '../../../assets/data/search';
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const DestinationSearchScreen = (props) => {
-  const [inputText, setInputText] = useState('');
+  // const [inputText, setInputText] = useState('');
   const navigation = useNavigation();
   return (
-      <View style={styles.container}>
-          {/* Input component */}
-          <TextInput 
-            style={styles.textInput}
-            placeholder ="어디 갈꺼니?"
-             value={inputText}
-             onChangeText={setInputText}
-          />
-          {/*List of Component */}
-          <FlatList 
-                data = {searchResults}
-                renderItem = {({item}) => (
-                  <Pressable onPress={()=>navigation.navigate('Guests')} style={styles.row}>
-                    <View style={styles.iconContainer}>
-                              <Entypo name = {"location-pin"} size ={30} />
-                    </View>
-                    <Text style = {styles.locationText}>{item.description}</Text>
-                  </Pressable>
+      // <View style={styles.container}>
+      //     {/* Input component */}
+      //     <TextInput 
+      //       style={styles.textInput}
+      //       placeholder ="어디 갈꺼니?"
+      //        value={inputText}
+      //        onChangeText={setInputText}
+      //     />
+      //     {/*List of Component */}
+      //     <FlatList 
+      //           data = {searchResults}
+      //           renderItem = {({item}) => (
+      //             <Pressable onPress={()=>navigation.navigate('Guests')} style={styles.row}>
+      //               <View style={styles.iconContainer}>
+      //                         <Entypo name = {"location-pin"} size ={30} />
+      //               </View>
+      //               <Text style = {styles.locationText}>{item.description}</Text>
+      //             </Pressable>
 
-                )
+      //           )
                     
-                }
-            />
-      </View>
-    // <View style={styles.container}>
-    //   <GooglePlacesAutocomplete
-    //     placeholder='Where are you going?'
-    //     onPress={(data, details = null) => {
-    //       // 'details' is provided when fetchDetails = true
-    //       console.log(data, details);
-    //       navigation.navigate('Guests', { viewport: details.geometry.viewport });
-    //     }}
-    //     fetchDetails
-    //     styles={{
-    //       textInput: styles.textInput,
-    //     }}
-    //     query={{
-    //       key: 'AIzaSyDYnkl8rGqpQ9-6XjrH3ssqNSDiJHpULGw',
-    //       language: 'en',
-    //       types: '(cities)',
-    //     }}
-    //     suppressDefaultStyles
-    //     renderRow={(item) => <SuggestionRow item={item} />}
-    //   />
-    // </View>
+      //           }
+      //       />
+      // </View>
+    <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder='Where are you going?'
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+          navigation.navigate('Guests', { viewport: details.geometry.viewport });
+        }}
+        fetchDetails
+        styles={{
+          textInput: styles.textInput,
+        }}
+        query={{
+          key: 'AIzaSyCyKXqiHJJPYdXylt4zxFmdW65-Jc1_sdE',
+          language: 'en',
+          types: '(cities)',
+        }}
+        suppressDefaultStyles
+        renderRow={(item) => <SuggestionRow item={item} />}
+      />
+    </View>
   );
 };
 
